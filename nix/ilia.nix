6528a933c9e1cba5b57aa7a5dc86cd0ef2641a42
Jesse Hallett <jesse@sitr.us>
2025-02-23 16:37:08 -0800

    flake with packaging for ilia

---
+++ nix/ilia.nix
@@ -0,0 +1,51 @@
+{ source # source must be given explicitly when calling callPackage
+
+  # callPackage automatically populates all of these arguments from nixpkgs
+, atk
+, cairo
+, gobject-introspection
+, gtk3
+, gtk-layer-shell
+, json-glib
+, lib
+, libgee
+, meson
+, ninja
+, pkg-config
+, stdenv
+, tinysparql
+, uncrustify
+, vala
+, wrapGAppsHook3
+}:
+
+stdenv.mkDerivation {
+  name = "ilia";
+  src = source;
+  postPatch = ''
+    patchShebangs meson_scripts
+  '';
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    uncrustify
+    vala
+    wrapGAppsHook3 # wraps the executable to register gsettings-schemas files
+  ];
+  buildInputs = [
+    atk
+    cairo
+    gobject-introspection # avoids error, "Package Gtk-3.0 not found in Vala API directories or Ggobject-introspection GIR directories"
+    gtk3
+    gtk-layer-shell
+    json-glib
+    libgee
+    tinysparql
+  ];
+  meta = {
+    homepage = "https://github.com/regolith-linux/ilia?tab=readme-ov-file";
+    license = [ lib.licenses.asl20 ];
+  };
+}
+
